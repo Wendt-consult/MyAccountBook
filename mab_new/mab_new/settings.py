2d4da265ab3dae5f68138529f62baf25c7cbe597
Roshan-maurya <roshan.maurya90@gmail.com>
2020-06-10 17:41:00 +0530

    sprint8(mail id created)

--- mab_new/mab_new/settings.py
+++ mab_new/mab_new/settings.py
@@ -134,7 +134,7 @@ LOGOUT_REDIRECT_URL = '/'
 #EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'akhilskarkera@gmail.com'
-EMAIL_HOST_PASSWORD = '9967373651a'
+EMAIL_HOST_USER = 'wendtinfosolution@gmail.com'
+EMAIL_HOST_PASSWORD = 'wendt@123'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
